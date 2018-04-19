aa4c8f74c0d7700355570587dc9b71ccbcfce2d1
Anwesh43 <anweshthecool0@gmail.com>
2018-04-19 09:23:04 +0530

    fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlinlinetozview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetozview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinlinetozview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.linetozview.LineToZView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToZView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
