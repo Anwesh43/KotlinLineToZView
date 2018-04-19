0180d6e0fedea410686a6fad963b187275f874f4
Anwesh43 <anweshthecool0@gmail.com>
2018-04-19 08:52:05 +0530

    adding View in activity

--- app/src/main/java/ui/anwesome/com/kotlinlinetozview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetozview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinlinetozview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.linetozview.LineToZView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LineToZView.create(this)
     }
 }
